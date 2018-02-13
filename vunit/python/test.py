dc28f83aa18f5f283bea06431014b4b8f3ad0507
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-02-13 20:04:00 +0100

    Moving python files to a subdirectory

--- vunit/python/test.py
+++ vunit/python/test.py
@@ -14,13 +14,13 @@ vu.add_com() # Add the communications library
 
 # Add all files ending in .vhd in current working directory to library
 lib_synth = vu.add_library("lib_synth")
-lib_synth.add_source_files("../vhdl/*.vhd")
+lib_synth.add_source_files("../../vhdl/*.vhd")
 lib_synth.add_compile_option("modelsim.vcom_flags", ["-check_synthesis"], allow_empty=True)
 
 lib_sim = vu.add_library("lib_sim")
-lib_sim.add_source_files("../vhdl/*.vhd")
-lib_sim.add_source_files("./vhdl/*_pkg.vhd")
-lib_sim.add_source_files("./vhdl/*_vunit_tb.vhd")
+lib_sim.add_source_files("../../vhdl/*.vhd")
+lib_sim.add_source_files("./../vhdl/*_pkg.vhd")
+lib_sim.add_source_files("./../vhdl/*_vunit_tb.vhd")
 
 # Run vunit function
 vu.main()
