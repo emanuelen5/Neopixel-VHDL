65427a3dd9498f9958545e72dcc46483e2c52560
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-05-08 21:23:27 +0200

    Removing vunit inclusion from synthesizable files

--- vhdl/neopixel_pkg.vhd
+++ vhdl/neopixel_pkg.vhd
@@ -2,9 +2,6 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
-library vunit_lib;
-context vunit_lib.vunit_context;
-
 package neopixel_pkg is
   subtype color_t is unsigned(7 downto 0);
   type rgb_color_t is record
